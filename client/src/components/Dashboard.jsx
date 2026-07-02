7ed036df47992488bba27870275e9da7e49cbc40
Sanika02025 <sanikagunjal2506@gmail.com>
2026-07-02 20:35:33 +0530

    dash1

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Task Tracker Pro 🚀</h1>
+      <p>Manage your daily tasks efficiently.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
